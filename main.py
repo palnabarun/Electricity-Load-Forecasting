54950c8ecef66f7bc47e88856484a7ddf3a8d645
Nabarun Pal <pal.nabarun95@gmail.com>
2017-05-03 22:34:05 +0530

    Adds main stub

---
+++ main.py
@@ -0,0 +1,11 @@
+from logs.logcontrol import *
+from metrics import *
+from preprocessor.prepare_household import *
+from preprocessor.splitter import *
+from models.rnn import build_rnn
+
+def run_main():
+    
+
+if __name__=="main()":
+    run_main()
